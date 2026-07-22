09493417e185f9c3fe6883dd9c13c375caf731dc
Madhumithakannan23 <kannanmadhumitha5@gmail.com>
2026-07-22 22:33:13 +0530

    Time: 51 ms (47.10%) | Memory: 22.7 MB (46.68%) - LeetSync

--- 72-edit-distance/edit-distance.py
+++ 72-edit-distance/edit-distance.py
@@ -4,7 +4,7 @@ class Solution:
 
         dp = [[0] * (n + 1) for _ in range(m + 1)]
 
-        # Base cases
+        
         for i in range(m + 1):
             dp[i][0] = i
         for j in range(n + 1):
@@ -16,9 +16,9 @@ class Solution:
                     dp[i][j] = dp[i - 1][j - 1]
                 else:
                     dp[i][j] = 1 + min(
-                        dp[i - 1][j],     # delete
-                        dp[i][j - 1],     # insert
-                        dp[i - 1][j - 1]  # replace
+                        dp[i - 1][j],     
+                        dp[i][j - 1],     
+                        dp[i - 1][j - 1]  
                     )
 
         return dp[m][n]
